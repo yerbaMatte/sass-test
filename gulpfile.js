087356dd426f61bd3c561e821cf472653c2b6ad8
Milosz Lewandowski <113758913+yerbaMatte@users.noreply.github.com>
2023-06-21 20:09:58 +0200

    nested style elements

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { watch, series, src, dest } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('*.scss').pipe(sass()).pipe(dest('css'));
+  return src('shinobi/**/*.scss').pipe(sass()).pipe(dest('css'));
 }
 
 function watchTask() {
-  watch(['*.scss'], buildStyles);
+  watch(['shinobi/**/*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
